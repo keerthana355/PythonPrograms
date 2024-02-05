5cf9120de97cd06f118db3ee96bd17447da2494b
keerthana <keerthanabanka3@gmail.com>
2024-02-05 15:03:35 +0530

    basics

---
+++ Type conversion.py
@@ -0,0 +1,15 @@
+x=2 #int
+y=12.3 #float
+z=1j #complex
+#covert from int to float
+a=float(x)
+#convert float to int
+b=int(y)
+#convert from int to complex
+c=complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
