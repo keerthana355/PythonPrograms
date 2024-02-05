0869bd0857b90049c286d157ebf2b0c4741e4d26
keerthana <keerthanabanka3@gmail.com>
2024-02-05 21:59:30 +0530

    collections

---
+++ List.py
@@ -0,0 +1,13 @@
+#to create a list
+list=["apple","grapes","guava"]
+print(list)
+#allow duplicates
+mylist=[2,5,9,8,9,2,4,4]
+print(mylist)
+#list length
+print(len(list))
+print(len(mylist))
+#list contain different datatypes
+list1=(1,"apple",True,False)
+print(list1)
+print(type(list1))
