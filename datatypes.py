82eaf4bedd2bb2c5d8171cdc5309af0d35403b3e
keerthana <keerthanabanka3@gmail.com>
2024-02-05 14:35:57 +0530

    basics

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+a="hello world" #string
+print(type(a))
+b=20 #integer
+print(type(b))
+c=20.5 #floating point
+print(type(c))
+d=2+4j #complex
+print(type(d))
+e=["apple","banana","cherry"] #list
+print(type(e))
+f=("apple","banana","cherry") #tuple
+print(type(f))
+g={"name":"john","age":36}
+print(type(g))
+h=range(6) #range
+print(type(h))
+i=True #boolean
+print(type(i))
+j=b"hello" #bytes
+print(type(j))
+k=bytearray(5) #bytesarray
+print(type(k))
+l=memoryview(bytes(5)) #memoryview
+print(type(l))
+m=None #None
+print(type(m))
+
+
