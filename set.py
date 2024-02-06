0101598192b065fe7fd0424af5bbaf074909fda4
keerthana <keerthanabanka3@gmail.com>
2024-02-06 15:36:38 +0530

    collections

---
+++ set.py
@@ -0,0 +1,12 @@
+#To create a set
+myset={"cat","dog","monkey"}
+print(type(myset))
+print(myset)
+#don't allow dulplicates
+set1={"dog","cat",1,True,"dog"}
+print(set1)
+#length of set
+print(len(set1))
+#set() constructor
+thisset=set((3,4,6,7,8,0))
+print(thisset)
