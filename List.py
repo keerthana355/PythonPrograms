5237046d8e2e6e1ec60234d487451aa9c2b55c51
keerthana <keerthanabanka3@gmail.com>
2024-02-06 13:24:55 +0530

    collections

--- List.py
+++ List.py
@@ -1,13 +1,53 @@
 #to create a list
-list=["apple","grapes","guava"]
+list1=["apple","grapes","guava","cherry","banana"]
 print(list)
 #allow duplicates
 mylist=[2,5,9,8,9,2,4,4]
 print(mylist)
 #list length
-print(len(list))
+print(len(list1))
 print(len(mylist))
 #list contain different datatypes
-list1=(1,"apple",True,False)
-print(list1)
+list2=(1,"apple",True,False)
+print(list2)
+#type()
 print(type(list1))
+#list() constructor
+thislist=list(("apple","cherry","banana"))
+print(thislist)
+#to print the first value
+print(list1[0])
+#negative indexing
+print(list1[-1])   #to print last element
+#Range of indexes
+print(list1[2:5])
+print(list1[:5])
+print(list1[1:])
+#change item value
+list1[1]="pomegranate"
+print(list1)
+print(list1[1])
+#append() method
+list1.append("orange")
+print(list1)
+#insert() method
+list1.insert(1,"strawberry")
+print(list1)
+#extend() method
+list3=["dog","cat","monkey"]
+list4=["rose","lilly","jasmine"]
+list3.extend(list4)
+print(list3)
+list4.extend(list3)
+print(list4)
+#remove() method
+list4.remove("lilly")
+print(list4)
+#pop() method
+list4.pop(1)
+print(list4)
+#delete 
+del list4
+#clear
+list1.clear()
+print(list1)
