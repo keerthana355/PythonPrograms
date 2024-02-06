61cc468e12050982cd84a05842e885e0e917d4f3
keerthana <keerthanabanka3@gmail.com>
2024-02-06 17:09:16 +0530

    collections

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+#to create dictionary
+mydictionary={"name":"monkey","age":2}
+print(type(mydictionary))
+print(mydictionary)
+print(mydictionary["age"])
+#duplicates not allowed
+mydictionary1={"name":"monkey","age":2,"age":3}
+print(type(mydictionary1))
+print(mydictionary1)
+#dictionary length
+print(len(mydictionary))
+#dict() constructor
+thisdict=dict(name="cat",age=2)
+print(thisdict)
+#get values
+x=mydictionary.get("name")
+print(x)
+#get keys
+y=mydictionary.keys()
+print(y)
+#get items
+z=mydictionary.items()
+print(z)
+#adding items
+mydictionary["colour"]="red"
+print(mydictionary)
+#copying a dictionary
+thisdictionary=mydictionary.copy()
+print(thisdictionary)
+#nested dictionaries
+mydict={"child1":{"name":"kali","year":2004},"child2":{"name":"centos","year":2006}}
+print(mydict)
+print(type(mydict))
