fe8c965e33ee7f579c6fcc3da6427b9945c63440
keerthana <keerthanabanka3@gmail.com>
2024-02-06 15:18:42 +0530

    collections

---
+++ Tuple.py
@@ -0,0 +1,22 @@
+#To create tuple
+tuple=("apple","banana","cherry")
+print(tuple)
+#allows duplicates
+tuple=("apple","banana","cherry","apple")
+print(tuple)
+#tuple length
+print(len(tuple))
+#one item tuple
+tuple1=("apple,")
+print(tuple1)
+print(type(tuple))
+#tuple can be any datatype
+tuple3=("apple",4,True,12.3)
+print(tuple3)
+#access tuple
+print(tuple[2])
+#negative indexing
+print(tuple3[-1])
+#range
+print(tuple[2:4])
+
