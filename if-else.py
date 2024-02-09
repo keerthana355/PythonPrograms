feef90f97a86d1eec7820a3bf0065385f596c7cb
keerthana <keerthanabanka3@gmail.com>
2024-02-09 13:20:20 +0530

    conditinal statements

---
+++ if-else.py
@@ -0,0 +1,15 @@
+#python program to get two number from the user and check first number is less than second number or not
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if a<b:
+ print(" a is less than b")
+else:
+ print("a is greater than b")
+
+#python program to check whether the given number is even or odd
+a=int(input("enter a number"))
+if (a%2)==0:
+ print("{} is even".format(a))
+else:
+ print("{} is odd".format(a))
