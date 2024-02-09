43d072fcb3bd9d5d899f5cce2a08d6bd3c9de416
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-09 22:03:39 +0530

    conditional statements

--- if-else.py
+++ if-else.py
@@ -1,17 +1,11 @@
                           Program-1
-#python program to get two number from the user and check first number is less than second number or not
+#python program to get two number from the user and check whether the first number is less than second number or not
 a=int(input("enter the value of a:"))
 b=int(input("enter the value of b:"))
 if a<b:
  print(" a is less than b")
 else:
  print("a is greater than b")
-<<<<<<< HEAD
-
-#python program to check whether the given number is even or odd
-a=int(input("enter a number"))
-if (a%2)==0:
-=======
  
 '''output:
 enter the value of a:3
@@ -21,18 +15,14 @@ a is less than b'''
 #python program to check whether the given number is even or odd
 a=intput("enter a number:")
 if a%2==0:
->>>>>>> 5f1c6f804fc61de70310c7d67fffe5f5ad3140b1
  print("{} is even".format(a))
 else:
  print("{} is odd".format(a))
-
-<<<<<<< HEAD
-=======
+  
 '''output:
 enter a number:6
 given number is even'''
                        Program-3
->>>>>>> 5f1c6f804fc61de70310c7d67fffe5f5ad3140b1
 #using and
 a=10
 b=20
@@ -42,7 +32,10 @@ if a>b and c<a:
 else:
  print("both conditions are false")
 
-<<<<<<< HEAD
+''' output:
+both conditions are false
+
+                      Program-4
 #using or
 a=10
 b=20
@@ -52,25 +45,10 @@ if a>b or c<a:
 else:
  print("no condition is true")
 
-=======
-#output:
-both conditions are false
-
-                       Program-4  
-#using or
-a=10
-b=20
-c=7
-if a>b or c<a:
- print("one condition is true")
-else:
- print("no conditions are true")
-
 '''output:
 one condition is true'''
 
-                       Program-5
->>>>>>> 5f1c6f804fc61de70310c7d67fffe5f5ad3140b1
+                    Program-5
 #using not
 a=10
 b=20
@@ -79,25 +57,15 @@ if not a>b:
 else:
  print("a is greater than b")
 
-<<<<<<< HEAD
-#Python program to check whether a person is eligible to vote or not
-age=int(input("Enter the age"))
-if age>=18:
-  print("You are eligible to vote")
-else:
-  print("You are not eligible to vote")
-
-=======
 '''output:
 a is not greater than b'''
 
-                       Program-6
+                     Program-6
 #Python program to check whether a person is eligible to vote or not
 age=int(input("Enter the age:"))
 if age>=18:
   print("You are eligible to vote")
- 
-else:
+ else:
   print("You are not eligible to vote")
 
 '''Output:
@@ -105,7 +73,6 @@ Enter the age:36
 You are eligible to vote'''
 
                       Program-7
->>>>>>> 5f1c6f804fc61de70310c7d67fffe5f5ad3140b1
 #Python program to find the largest number of the given three numbers
 a=19
 b=30
@@ -116,9 +83,6 @@ if b<a and b>c:
   print("b is the largest number")
 if c>a and c>b:
   print("c is the largest number")
-<<<<<<< HEAD
-=======
 
 '''Output:
 c is the largest number'''
->>>>>>> 5f1c6f804fc61de70310c7d67fffe5f5ad3140b1
