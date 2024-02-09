af114a83e6a207f3d9f527ad34aeab4c3cdf52fc
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-09 12:53:34 +0530

    if-else

--- conditional statements.py
+++ conditional statements.py
@@ -1,7 +1,13 @@
-a=int(input("enter the value of a"))
-b=int(input("enter the value of b"))
+#python program to get two number from the user and check first number is less than second number or not
+
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
 if a<b:
  print(" a is less than b")
 else:
  print("a is greater than b")
- 
+
+output:
+enter the value of a:3
+enter the value of b:7
+a is less than b
