dc38c3fbf8b8ec6ee33854d7fd079f848e268f9a
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-09 14:12:00 +0530

    conditional statements.py

--- if-else.py
+++ if-else.py
@@ -7,10 +7,10 @@ if a<b:
 else:
  print("a is greater than b")
  
-output:
+'''output:
 enter the value of a:3
 enter the value of b:7
-a is less than b
+a is less than b'''
 
 #python program to check whether the given number is even or odd
 a=intput("enter a number:")
@@ -19,42 +19,9 @@ if a%2==0:
 else:
  print("{} is odd".format(a))
 
-output:
+'''output:
 enter a number:6
-given number is even
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#python program to check whether the given number is even or odd
-a=int(input("enter a number"))
-if (a%2)==0:
- print("{} is even".format(a))
-else:
- print("{} is odd".format(a))
-<<<<<<< HEAD
+given number is even'''
 
 #using and
 a=10
@@ -64,5 +31,29 @@ if a>b and c<a:
  print("both conditions are true")
 else:
  print("both conditions are false")
-=======
->>>>>>> 05b286af7214c1472e305ffb52031adc63d9e7c0
+
+#output:
+both conditions are false
+
+#using or
+a=10
+b=20
+c=7
+if a>b or c<a:
+ print("one condition is true")
+else:
+ print("no conditions are true")
+
+'''output:
+one condition is true'''
+
+#using not
+a=10
+b=20
+if not a>b:
+ print("a is not greater than b")
+else:
+ print("a is greater than b")
+
+'''output:
+a is not greater than b'''
