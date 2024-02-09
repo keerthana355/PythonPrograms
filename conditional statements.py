2cf4a3ec8e039d9e187530a5225dbf33cf262576
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-09 12:58:34 +0530

    if-else

--- conditional statements.py
+++ conditional statements.py
@@ -11,3 +11,14 @@ output:
 enter the value of a:3
 enter the value of b:7
 a is less than b
+
+#python program to check whether the given number is even or odd
+a=intput("enter a number:")
+if a%2==0:
+ print("given number is even")
+else:
+ print("given number is odd")
+
+output:
+enter a number:6
+given number is even
