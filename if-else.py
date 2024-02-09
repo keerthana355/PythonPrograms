fe609a92da8c7dfb3cc6d36279c153219d56bdc8
keerthana <keerthanabanka3@gmail.com>
2024-02-09 21:22:32 +0530

    conditional statements

--- if-else.py
+++ if-else.py
@@ -6,47 +6,6 @@ if a<b:
  print(" a is less than b")
 else:
  print("a is greater than b")
- 
-output:
-enter the value of a:3
-enter the value of b:7
-a is less than b
-
-#python program to check whether the given number is even or odd
-a=intput("enter a number:")
-if a%2==0:
- print("{} is even".format(a))
-else:
- print("{} is odd".format(a))
-
-output:
-enter a number:6
-given number is even
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #python program to check whether the given number is even or odd
 a=int(input("enter a number"))
@@ -54,7 +13,6 @@ if (a%2)==0:
  print("{} is even".format(a))
 else:
  print("{} is odd".format(a))
-<<<<<<< HEAD
 
 #using and
 a=10
@@ -64,5 +22,38 @@ if a>b and c<a:
  print("both conditions are true")
 else:
  print("both conditions are false")
-=======
->>>>>>> 05b286af7214c1472e305ffb52031adc63d9e7c0
+
+#using or
+a=10
+b=20
+c=5
+if a>b or c<a:
+ print("one condition is true")
+else:
+ print("no condition is true")
+
+#using not
+a=10
+b=20
+if not a>b:
+ print("a is not greater than b")
+else:
+ print("a is greater than b")
+
+#Python program to check whether a person is eligible to vote or not
+age=int(input("Enter the age"))
+if age>=18:
+  print("You are eligible to vote")
+else:
+  print("You are not eligible to vote")
+
+#Python program to find the largest number of the given three numbers
+a=19
+b=30
+c=100
+if a>b and a>c:
+  print("a is the largest number")
+if b<a and b>c:
+  print("b is the largest number")
+if c>a and c>b:
+  print("c is the largest number")
