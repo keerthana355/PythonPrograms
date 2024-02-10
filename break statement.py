873dddeb8a7d4e1f1d74eecf6bb76e4e3080546b
keerthana <keerthanabanka3@gmail.com>
2024-02-10 22:14:10 +0530

    jumping statements

---
+++ break statement.py
@@ -0,0 +1,16 @@
+           #Program-1
+#Python program to illustrate break statement
+for i in range(1,10):
+ if i==7:
+  print("processing is enough")
+  break
+ print(i)
+  
+           #Program-2
+fruits=["apple","banana","cherry"]
+for x in fruits:
+ if x=="banana":
+  break
+ print(x)
+
+
---
+++ continue statement.py
@@ -0,0 +1,13 @@
+          #Program-1
+#Python program to illustrate continue statement
+for i in range(0,10):
+ if(i==7):
+  continue
+ print(i)
+
+         #Program-2
+fruits=["apple","banana","cherry","guava","strawberry","mango"]
+for x in fruits:
+ if(x=="strawberry"):
+  continue
+ print(x)
