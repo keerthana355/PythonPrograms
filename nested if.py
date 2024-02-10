f99e7f4c7af3ac656eef32dde96157c5842e1aa1
keerthana <keerthanabanka3@gmail.com>
2024-02-10 11:21:26 +0530

    conditional statements

--- nested if.py
+++ nested if.py
@@ -1,8 +1,23 @@
-a=int(input("enter a "))
-b=int(input("enter b"))
-if a<b:
-  print("a is less than b")
-if a%2==0:
-    print("a is even")
+#Python program to demonstrate nested if statement
+a=99
+if a<100:
+   print("a is less than 100")
+   if a%2==0: 
+         print("a is even")
+   else:
+         print("a is odd")
 else:
-    print("a is odd")
+ print("a is greater than 100")
+
+
+num=59
+if num%2==0:
+   if num%3==0:
+      print("Divisible by 2 and 3")
+   else:
+      print("Divisible by 2 and not divisble by 3")
+else:
+    if num%3==0:
+      print("Divisible by 3 and not divisble by 2")
+    else:
+      print("Not divisble by 2 and and not divisble by 3")
