733bb42df7d9392b27eb3b82a86ec5d61506af82
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-10 22:19:25 +0530

    Jumping statements

--- continue statement.py
+++ continue statement.py
@@ -5,9 +5,28 @@ for i in range(0,10):
   continue
  print(i)
 
+'''Output:
+0
+1
+2
+3
+4
+5
+6
+8
+9'''
+
          #Program-2
 fruits=["apple","banana","cherry","guava","strawberry","mango"]
 for x in fruits:
  if(x=="strawberry"):
   continue
  print(x)
+
+'''Output:
+apple
+banana
+cherry
+guava
+mango'''
+          
