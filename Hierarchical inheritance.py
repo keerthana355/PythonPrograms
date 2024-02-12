34073e4293b3e218998fd1ff8425e56cc8e83cfc
keerthana <keerthanabanka3@gmail.com>
2024-02-12 05:48:18 +0530

    OOPS Concepts

---
+++ Hierarchical inheritance.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def info(self):
+        print("This is Vehicle")
+class Car(Vehicle):
+    def car_info(self,name):
+        print("Car name is:", name)
+class Truck(Vehicle):
+    def truck_info(self,name):
+        print("Truck name is:",name)
+obj1 = Car()
+obj1.info()
+obj1.car_info('BMW')
+
+obj2 = Truck()
+obj2.info()
+obj2.truck_info('Ford')
+
+'''Output:
+This is Vehicle
+Car name is: BMW
+This is Vehicle
+Truck name is: Ford '''
