e1ebc11a9cc6c70653af951d2baebfac99b9f058
banka-keerthana <158732131+banka-keerthana@users.noreply.github.com>
2024-02-18 16:49:07 +0530

    Modules

--- Modules.py
+++ Modules.py
@@ -5,6 +5,10 @@ import math
 print(math.e)
 print(math.pi)
 
+'''Output:
+2.718281828459045
+3.141592653589793 '''
+
 #importing user defined module
 
 import My_module
@@ -12,16 +16,42 @@ print(My_module.a)
 print(My_module.area_of_square(9))
 print(My_module.calculator(3,5))
 
+'''Output:
+10
+81
+Addition is: 8
+Subraction is: -2
+Multiplication is 15
+Division is: 0.6
+None '''
+
 #importing user defined module as shortcut name
 import My_module as m
 print(m.a)
 print(m.area_of_square(7))
 print(m.calculator(5,8))
+
+'''Output:
+10
+49
+Addition is: 13
+Subraction is: -3
+Multiplication is 40
+Division is: 0.625
+None '''
  
 #importing particular function from our module
 from My_module import a,calculator
 print(a)
 print(calculator(12,10))
+
+'''Output:
+10
+Addition is: 22
+Subraction is: 2
+Multiplication is 120
+Division is: 1.2
+None '''
 
 #importing all functions from user defined module
 #from My_module import *
@@ -30,5 +60,7 @@ b=4
 sum=a+b
 print("value of a from another module",My_module.a)
 
+'''Output:
+value of a from another module 10 '''
 
 
